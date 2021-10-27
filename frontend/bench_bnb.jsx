f37dc044cee28b42532f86ba81ff4b7e72a47a68
mrossant <matteorossant@gmail.com>
2021-10-27 10:49:57 -0400

    configured the store and tested state on the window

--- frontend/bench_bnb.jsx
+++ frontend/bench_bnb.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { postUser, postSession, deleteSession } from './util/session_api_util';
+import configureStore from './store/store';
 
 document.addEventListener("DOMContentLoaded", () => {
   const root = document.getElementById("root");
+  const store = configureStore();
+  window.getState = store.getState;
+  window.dispatch = store.dispatch;
   window.postUser = postUser;
   window.postSession = postSession;
   window.deleteSession = deleteSession;
--- frontend/reducers/session_errors_reducer.js
+++ frontend/reducers/session_errors_reducer.js
@@ -1,15 +1,13 @@
 import { RECEIVE_CURRENT_USER, RECEIVE_ERRORS } from "../actions/session_actions";
 
-const _initialErrorState = {
-    session: []
-}
+const _initialErrorState = [];
 
 const sessionsErrorsReducer = (state = _initialErrorState, action) => {
     Object.freeze(state);
 
     switch (action.type) {
         case RECEIVE_ERRORS:
-            return Object.assign({}, { session: action.errors });
+            return action.errors;
         case RECEIVE_CURRENT_USER:
             return _initialErrorState;
         default:
