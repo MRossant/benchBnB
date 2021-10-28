87c6bd67c27bf96ecb8cd9ad1c03ddd014383707
mrossant <matteorossant@gmail.com>
2021-10-28 10:49:45 -0400

    unresolved currentUser issue

--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -1,22 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-export default ({ currentUser, logout }) => {
-    const display = currentUser ? (
+class Greeting extends React.Component {
+
+    render () {
+        const { currentUser, logout } = this.props;
+
+        const display = currentUser ? (
         <div>
             <p>Greeting, {currentUser.username}!</p>
             <button onClick={logout}>Log Out</button>
         </div>
-    ) : (
+        ) : (
         <div>
             <Link className='btn' to="/signup">Sign Up</Link>
             <Link className='btn' to="/login">Log In</Link>
         </div>
-    );
+        );
 
-    return (
+        return (
         <div>
             {display}
         </div>
-    )
+        )
+    }
 }
+
+export default Greeting;
--- frontend/components/greeting/greeting_container.js
+++ frontend/components/greeting/greeting_container.js
@@ -2,15 +2,15 @@ import Greeting from './greeting';
 import { connect } from 'react-redux';
 import { login, logout, signup } from '../../actions/session_actions';
 
-const mSTP = state => {
-    debugger
+const mSTP = (state) => {
+    const sessionId = state.session.id
     return {
-        currentUser: state.entities.users[state.session.id]
+        currentUser: state.entities.users[sessionId]
     }
 };
 
 const mDTP = dispatch => ({
-    login: user => dispatch(login(user)),
+    logout: user => dispatch(logout(user)),
 });
 
 export default connect(mSTP, mDTP)(Greeting);
